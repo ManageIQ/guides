23eb4d2489bc7d1fcd677d25e5da0eef557055d0
Jason Frey <fryguy9@gmail.com>
2020-05-15 16:47:16 -0400

    Consolidate triage meetings

--- bin/generate_triage_links.rb
+++ bin/generate_triage_links.rb
@@ -1,12 +1,16 @@
 #! /usr/bin/env ruby
 
 require "pathname"
+require "uri"
 require "yaml"
 
 params = YAML.load_file(Pathname.new(__dir__).join("triage.yml"))
 
+named_sections = params["repos"].size > 1
+
 links = params["repos"].product(params["links"]).map do |repo, link|
-  header = "[#{repo["name"]} #{link["name"]}]:".ljust(60, " ")
+  repo_name = named_sections ? "#{repo["name"]} " : ""
+  header = "[#{repo_name}#{link["name"]}]:".ljust(60, " ")
 
   params = (link["params"] + repo["params"]).flatten.join(" ")
   url    = URI::HTTPS.build(:host => "github.com", :path => "/issues", :query => URI.encode_www_form(:q => params))
